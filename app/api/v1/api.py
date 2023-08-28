29bf678a5ea953641b4795608f939299af316266
Rene Moser <mail@renemoser.net>
2023-08-28 13:28:10 +0200

    add exoscale

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from . import cloudscale_ch
+from . import cloudscale_ch, exoscale
 
 api_router = APIRouter()
 api_router.include_router(cloudscale_ch.router, prefix="/cloudscale-ch", tags=["cloudscale.ch"])
+api_router.include_router(exoscale.router, prefix="/exoscale", tags=["exoscale"])
---
+++ app/model/exoscale.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class Zone(str, Enum):
+    CH_GVA_2 = "ch-gva-2"
+    CH_DK_2 = "ch-dk-2"
+    DE_FRA_1 = "de-fra-1"
+    DE_MUC_1 = "de-muc-1"
+    AT_VIE_1 = "at-vie-1"
+    BG_SOF_1 = "bg-sof-1"
